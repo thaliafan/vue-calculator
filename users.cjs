f9730b67f572377bfa7bcc3ec5ac82d3e3d40438
Thalia <thalia@email.com>
2025-08-05 10:14:41 +1000

    New User

--- users.cjs
+++ users.cjs
@@ -2,9 +2,9 @@
 const users = [
   {
     id: 1,
-    username: 'admin',
+    username: 'TF',
     role: 'admin',
-    password_hash: '$2b$10$dflFu5cDJYaYGwZD0MhB1efoOiRUDC7YXivxZX6IURNBNbR8.cIVa'
+    password_hash: '$2b$10$pA3aFOD7PfxE4/Yyqle3I.aiVm6MZ2XrOloDnuR9N8EYzajr5SX6q'
   }, // 👈 修正1：在这里添加了逗号
 
   // 👇 修正2：用花括号 { } 把新用户信息包起来
